c7b0cfa13fd85a9022b2c5f954343dc5b31701ff
Daniil-l17 <129774580+Daniil-l17@users.noreply.github.com>
2024-10-30 18:26:44 +0500

    добавил component item

---
+++ src/page/Post/components/CommentItem/types.tsx
@@ -0,0 +1,5 @@
+import { Comment } from '../../../Home/services';
+
+export type PropsCommentItem = {
+	comment: Comment;
+};
--- src/page/Post/components/CommentWrapper/index.tsx
+++ src/page/Post/components/CommentWrapper/index.tsx
@@ -1,57 +1,13 @@
-import { FC, useState } from 'react';
+import { FC } from 'react';
 import { PropsCommentWrapper } from './types';
-import {
-	$ContentDescription,
-	$ContentItem,
-	$CommentWrapper,
-	$ContentItemHeartIconWpapperContext,
-	$ContentItemHeader,
-	$ContentItemHeartIconWrapper,
-	$ContentItemHeaderDeleteIcon,
-	$ContentItemHeaderUserInfo,
-	$ContentItemHeaderUserInfoDate,
-	$ContentItemHeaderUserInfoName
-} from './style';
-import { Link } from 'react-router-dom';
-import { Avatar, Loader } from '@mantine/core';
-import { RiDeleteBin6Line } from 'react-icons/ri';
-import { motion } from 'framer-motion';
-import { useAuth } from '../../../../hooks/userAuth';
+import { $CommentWrapper } from './style';
+import { CommentItem } from '../CommentItem';
+
 export const CommentWrapper: FC<PropsCommentWrapper> = ({ comments }) => {
-	const [deleteId, setDeleteId] = useState('');
-	let isPendingDelete = false;
-	const { user } = useAuth();
 	return (
 		<$CommentWrapper>
 			{comments.map(comment => (
-				<motion.div whileHover={{ scale: 1.02, transition: { duration: 0.3 }, opacity: 0.75 }} transition={{ duration: 0.4 }}>
-					<div style={{ position: 'relative', width: '100%' }}>
-						<$ContentItemHeaderDeleteIcon>
-							{user?.id === comment.user.id ? (
-								isPendingDelete && deleteId === comment.id ? (
-									<Loader size={20} />
-								) : (
-									<motion.div whileHover={{ scale: 1.2, transition: { duration: 0.3 } }} transition={{ duration: 0.5 }}>
-										<RiDeleteBin6Line style={{ cursor: 'pointer', zIndex: 2, fontSize: '17px' }} />
-									</motion.div>
-								)
-							) : null}
-						</$ContentItemHeaderDeleteIcon>
-						<$ContentItem>
-							<$ContentItemHeader>
-								<Link style={{ display: 'inline-block' }} to={`/users/${comment.user.id}`}>
-									<$ContentItemHeaderUserInfo>
-										<Avatar size='md' src={`${import.meta.env['VITE_BAC_STORE-IMG_URL']}${comment.user.avatarUrl}`} alt="it's me" />
-										<div>
-											<$ContentItemHeaderUserInfoName>{comment.user.name}</$ContentItemHeaderUserInfoName>
-										</div>
-									</$ContentItemHeaderUserInfo>
-								</Link>
-								<$ContentDescription>{comment.content}</$ContentDescription>
-							</$ContentItemHeader>
-						</$ContentItem>
-					</div>
-				</motion.div>
+				<CommentItem key={comment.id} comment={comment} />
 			))}
 		</$CommentWrapper>
 	);
